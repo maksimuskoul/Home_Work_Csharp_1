b915db092bb296facc324e95faa1cfcb6ddc38c1
Maksim <maksimkon2012945@gmail.com>
2023-07-05 23:23:31 +0300

    почти решил 4

--- Zadacha_4/Program.cs
+++ Zadacha_4/Program.cs
@@ -3,3 +3,43 @@
 5 -> 2, 4
 8 -> 2, 4, 6, 8
 */
+using System;
+
+namespace Zadacha_4 
+    {
+        class program
+        {
+            static void Main(string[]args)
+            {
+                Console.WriteLine("Введите число");
+
+                int nam_A = Convert.ToInt32(Console.ReadLine());
+                
+                int count = 1;
+                while(count<=nam_A)
+                {
+                    if(count/2==0)
+                    {
+                        count++;
+                        Console.Write(count+" ");
+                    }
+                    
+
+                    
+                }
+                
+                    
+                    
+                    
+                    
+               
+            }
+        }
+        
+    }   
+
+
+
+    /*Console.WriteLine("Введите число");
+    int nam_A = Convert.ToInt32(Console.ReadLine());
+    int array[Console.ReadLine()]*/
