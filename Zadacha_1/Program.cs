8a2d3ca3c117e7320615b7279d367e58cc6757d6
Maksim <maksimkon2012945@gmail.com>
2023-07-05 11:18:32 +0300

    1 Задача готова

--- Zadacha_1/Program.cs
+++ Zadacha_1/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+using System;
+
+namespace Zadacha_1 
+    {
+        class program
+        {
+            static void Main(string[]args)
+            {
+                Console.WriteLine("напишите первое число");
+                int nam_A = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("напишите второе число");
+                int nam_B = Convert.ToInt32(Console.ReadLine());
+
+                int max = 0;
+
+                    if (nam_A > nam_B)
+                    {
+                        max = nam_A;
+                        Console.WriteLine("первое  число больше");
+                    }
+                        else 
+                        {
+                            max = nam_B;
+                            Console.WriteLine("второе  число больше");
+                        }
+                    
+
+            }
+        }
+        
+    }   
